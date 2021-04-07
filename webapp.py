7fb75dccfdfc9fb7f7dc275456169b59476486c3
Flan-231 <77691192+Flan-231@users.noreply.github.com>
2021-04-07 09:31:16 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,15 @@
 
+from flask import Flask, request, Markup, render_template, flash
+from datetime import datetime
+
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_about():
+    return render_template('about.html')
+  
+  if __name__ == '__main__':
+    app.run(debug=False) # change to False when running in production
