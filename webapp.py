0312ef03ae99e730c0ebe37fdca95864c39f64fc
Flan-231 <77691192+Flan-231@users.noreply.github.com>
2021-04-08 09:14:45 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,6 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 def render_about():
     return render_template('home.html')
-  
- if __name__ == '__main__':
+
+if __name__ == '__main__':
     app.run(debug=False) # change to False when running in production
